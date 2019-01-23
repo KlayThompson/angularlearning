47ac3ab3576a449e78e0663b90c042f19a89dcaa
KlayThompson <913661647@qq.com>
2019-01-23 14:40:36 +0800

    install jquery & bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { ProductComponent } from './product/product.component';
+import { StarsComponent } from './stars/stars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    FooterComponent,
+    BannerComponent,
+    ProductComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule
